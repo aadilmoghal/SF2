898a1276e26bffc8f7ffb7f92c94cef4fa93b799
Ubuntu <ubuntu@ip-172-31-41-208.eu-west-2.compute.internal>
2021-04-06 16:29:35 +0000

    ec2 vpc fix

--- terraform/EC2/main.tf
+++ terraform/EC2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "Jenkins_Build_VM" {
  instance_type          = var.instance_type    
  key_name               = "aws-key"
  subnet_id              = var.subnet_id
- vpc_security_group_ids = var.security_group_ids
+ vpc_security_group_ids = var.vpc_security_group_ids
 
  lifecycle {
      create_before_destroy = true
