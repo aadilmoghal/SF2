3680876b965ba0a8a3b57d3ce13f317c1b42e70f
Ubuntu <ubuntu@ip-172-31-41-208.eu-west-2.compute.internal>
2021-04-08 17:48:29 +0000

    fix

--- jenkins-scripts/push.sh
+++ jenkins-scripts/push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh -i /home/jenkins/.ssh/id_rsa ubuntu@3.10.205.80 << EOF
+ssh -i /home/jenkins/.ssh/id_rsa ubuntu@18.133.226.200<< EOF
 
 docker push aadilmoghal/front_end:latest
 docker push aadilmoghal/back_end:latest
