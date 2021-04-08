f2bb80cb2d94036a188a4fe4400414dc12251e34
Ubuntu <ubuntu@ip-172-31-41-208.eu-west-2.compute.internal>
2021-04-08 23:18:26 +0000

    update dep.sh

--- jenkins-scripts/dep.sh
+++ jenkins-scripts/dep.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # kubectl delete all --all -n SFIA2
-cd kubernetes/
+cd ../kubernetes/
 kubectl delete pods --all -n SFIA2
 kubectl apply -f ns.yaml
 kubectl apply -f conf.yaml
-kubectl apply -f load-balancer.yaml 
+kubectl create -f load-balancer.yaml 
 kubectl apply -f nginx.yaml
-kubectl apply -f frontend-dep.yaml
-kubectl apply -f frontend-cluster-serv
+kubectl create -f frontend-dep.yaml
+kubectl create -f frontend-cluster-serv.yaml
 kubectl apply -f backend-cluster.yaml
 kubectl apply -f backend-dep.yaml
 kubectl describe service load-balancer -n SFIA2
