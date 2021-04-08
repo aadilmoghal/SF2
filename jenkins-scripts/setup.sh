fac6d79da24747007d8ee47251ecc6c171eb2bf0
Ubuntu <ubuntu@ip-172-31-41-208.eu-west-2.compute.internal>
2021-04-08 13:46:33 +0000

    no chmod

--- jenkins-scripts/setup.sh
+++ jenkins-scripts/setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-chmod 400 ~/.ssh/id_rsa
 ssh -i ~/.ssh/id_rsa ubuntu@3.10.205.80 << EOF
 
 sudo rm -r SF2
@@ -10,8 +9,6 @@ cd SF2/
 
 docker-compose up -d --build
 
-docker exec backend bash -c "pytest tests/ --cov application" >>
-docker exec frontend bash -c "pytest tests/ --cov application" >> 
 
 docker-compose down
 
