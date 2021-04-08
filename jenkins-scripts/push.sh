ff4975ae0ba63348ef5a76c6fbcce56cda8078b1
Ubuntu <ubuntu@ip-172-31-41-208.eu-west-2.compute.internal>
2021-04-08 19:45:05 +0000

    push

--- jenkins-scripts/push.sh
+++ jenkins-scripts/push.sh
@@ -2,7 +2,17 @@
 
 ssh -i /home/jenkins/.ssh/id_rsa ubuntu@18.133.226.200 << EOF
 
+cd SF2/
+
+git checkout kubernetes
+
+cd frontend
 sudo docker push aadilmoghal/front_end:latest
+
+cd ..
+
+cd backend
+
 sudo docker push aadilmoghal/back_end:latest
 
 EOF
