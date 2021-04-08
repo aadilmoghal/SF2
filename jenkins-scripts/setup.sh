74ca5341ed7edef431e87a8dd50fbd5aa0a489e8
aadilmoghal <78487765+aadilmoghal@users.noreply.github.com>
2021-04-08 12:12:09 +0100

    Update setup.sh

---
+++ jenkins-scripts/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ssh -i /home/jenkins/.ssh/id_rsa ubuntu@3.10.205.80 << EOF
+
+sudo rm -r SF2
+
+git clone https://github.com/aadilmoghal/SF2.git
+
+cd SF2/
+
+docker-compose up -d --build
+
+docker exec backend bash -c "pytest tests/ --cov application" >> backend_test.txt
+docker exec frontend bash -c "pytest tests/ --cov application" >> frontend_test.txt
+
+docker-compose down
+
+EOF
