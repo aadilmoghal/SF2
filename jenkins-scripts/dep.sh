521d82e9236ac2ed0b5dc6d2e9304a8d24c1f357
Ubuntu <ubuntu@ip-172-31-41-208.eu-west-2.compute.internal>
2021-04-09 10:51:59 +0000

    push was needed

--- jenkins-scripts/dep.sh
+++ jenkins-scripts/dep.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # first need to go into yaml folder
 cd kubernetes/
-#need to create in order, first with name space creation
+# need to create in order, first with name space creation
 kubectl apply -f ns.yaml
-#create nginx config file
+# create nginx config file
 kubectl apply -f conf.yaml
-#launch up the load balancer with the serv files
+# launch up the load balancer with the serv files
 kubectl create -f load-balancer.yaml 
 kubectl apply -f nginx.yaml
 kubectl create -f frontend-dep.yaml
-#launch up the dep files
+# launch up the dep files
 kubectl create -f frontend-cluster-serv.yaml
 kubectl apply -f backend-cluster.yaml
 kubectl apply -f backend-dep.yaml
